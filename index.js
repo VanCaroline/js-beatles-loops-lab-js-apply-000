ef1ee3280e9a12f62155be40f626ca396662bc3e
Vanessa Porras <vanessapcg@gmail.com>
2020-10-22 22:20:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments)
+{
+  
+  var array=[];
+
+    for (var i=0; i<musicians.length; i++)
+    {
+        array[i]=musicians[i]+' plays '+instruments[i];
+    }  
+  
+ return array;
+}
+
+
+
+function johnLennonFacts (facts){
+  
+  var a=0;
+  
+  while (a<facts.length)
+  {
+    consolo.log("HIIIIIII");
+   // facts[a]=facts[a]+'!!!'
+   a++;
+  }
+  
+  return facts;
+}
